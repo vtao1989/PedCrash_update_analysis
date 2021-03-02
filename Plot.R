55a3077d1c9b4362f9a208820a656a82b2eeb517
Tao <30710905+vtao1989@users.noreply.github.com>
2021-03-02 16:33:56 -0600

    add comparison plots

--- Plot.R
+++ Plot.R
@@ -3,6 +3,11 @@
 
 library(tidyverse)
 library(sf)
+library(gridExtra)
+library(grid)
+library(rlang)
+library(ggspatial)
+library(ggmap)
 
 ## intersection
 intersection = st_read('C:/UMN/Projects/PedCrash/PedCrash_GIS/Reproduction_of_crash_model_construction/Intersections.shp',
@@ -13,15 +18,176 @@ intersection = st_read('C:/UMN/Projects/PedCrash/PedCrash_GIS/Reproduction_of_cr
 ## mid-block
 city_network = st_read('C:/UMN/Projects/PedCrash/PedCrash_GIS/Reproduction_of_crash_model_construction/Road_network_with_highway.shp',
                        stringsAsFactors = F) %>% 
-  select(OFT, STREET_TYP) %>% 
-  st_zm() %>% 
-  mutate(STREET_TYP = ifelse(is.na(STREET_TYP), 'Local', STREET_TYP))
+  select(OFT) %>% 
+  st_zm()
 
-## connection between intersection and mid-block
-int_block = st_read('C:/UMN/Projects/PedCrash/PedCrash_GIS/Reproduction_of_crash_model_construction/Intersection_intersect_with_block.shp',
-                    stringsAsFactors = F) %>% 
-  st_drop_geometry() %>% 
-  select(JuncID, OFT)
+## city boundary, ACP50 boundary, CBD boundary
+city_bdry <- st_read('C:/UMN/Projects/PedCrash/PedCrash_GIS/Reproduction_of_crash_model_construction/City_boundary.shp')
+acp_bdry <- st_read('C:/UMN/Projects/PedCrash/PedCrash_GIS/Reproduction_of_crash_model_construction/ACP50_boundary.shp')
+CBD_bdry <- st_read('C:/UMN/Projects/PedCrash/PedCrash_GIS/Reproduction_of_crash_model_construction/Downtown_boundary_dissolved.shp')
+
+## data
+new_intersection_result = read.csv('./result/new_intersection_result.csv',
+                                   stringsAsFactors = F)
+new_block_result = read.csv('./result/new_block_result.csv',
+                            stringsAsFactors = F)
+
+# high-risk location plot -------------------------------------------------
+
+# get legend of the ggplot
+get_legend<-function(myggplot){
+  tmp <- ggplot_gtable(ggplot_build(myggplot))
+  leg <- which(sapply(tmp$grobs, function(x) x$name) == "guide-box")
+  legend <- tmp$grobs[[leg]]
+  return(legend)
+}
+
+# compare function
+compare <- function(person, location){
+  
+  ## dataset
+  if(location == 'int'){
+    data = new_intersection_result
+    ID = data$JuncID
+  } else {
+    data = new_block_result
+    ID = data$OFT
+  }
+  
+  ## criteria for selection
+  crit = nrow(data) * 0.01
+  
+  ## model and prediction
+  model_loc = paste('./result/', person, '_', location, '_m.RDS', sep = '')
+  m = readRDS(model_loc)
+  pre = predict(m, data, type = 'response')
+  
+  no_exp_model_loc = paste('./result/', person, '_', location, '_no_exp.RDS', sep = '')
+  no_exp = readRDS(no_exp_model_loc)
+  no_exp_pre = predict(no_exp, data, type = 'response')
+  
+  ## combine prediction result
+  result = cbind(ID,
+                 pre,
+                 no_exp_pre) %>% 
+    as_tibble() %>% 
+    mutate(rank = row_number(desc(pre)),
+           rank_no_exp = row_number(desc(no_exp_pre))) %>% 
+    mutate(both = ifelse(rank <= crit & rank_no_exp <= crit, 1, 0)) %>% 
+    select(ID, rank, rank_no_exp, both)
+  
+  ## merge result to map
+  if(location == 'int'){
+    map = intersection %>% 
+      left_join(result, by = c('JuncID'='ID'))
+  } else {
+    map = city_network %>% 
+      left_join(result, by = c('OFT'='ID'))
+  }
+  
+  ## select results for plotting
+  plot_data <- map %>%
+    filter(rank <= crit) %>% 
+    mutate(both = ifelse(both == 1, 'Same Location', 'Different Location'))
+  
+  ## titles of subplots
+  if(person == 'ped') {
+    if (location == 'int') {
+      title1 = '(a) Pedestrian Intersection Model with Exposure'
+      title2 = '(b) Pedestrian Intersection Model without Exposure'
+    } else {
+      title1 = '(c) Pedestrian Mid-block Model with Exposure'
+      title2 = '(d) Pedestrian Mid-block Model without Exposure'
+    }
+  } else {
+    if (location == 'int') {
+      title1 = '(a) Bicycle Intersection Model with Exposure'
+      title2 = '(b) Bicycle Intersection Model without Exposure'
+    } else {
+      title1 = '(c) Bicycle Mid-block Model with Exposure'
+      title2 = '(d) Bicycle Mid-block Model without Exposure'
+    }
+  }
+  
+  ## left figure (with exposure)
+  p1 <- get_googlemap('Minneapolis', zoom = 11,
+                      maptype = "roadmap",
+                      style = c(feature = "all", element = "labels", visibility = 'off'),
+                      color = "bw") %>%
+    ggmap() +
+    geom_sf(data = plot_data, aes(col = factor(both)), inherit.aes =  FALSE, size = 1, key_glyph = draw_key_rect) +
+    coord_sf(crs = st_crs(4326)) +
+    geom_sf(data = city_bdry, fill = NA, aes(col = 'City Boundary'), inherit.aes = FALSE) +
+    coord_sf(crs = st_crs(4326)) +
+    geom_sf(data = acp_bdry, fill = NA, aes(col = 'ACP50'), inherit.aes = FALSE) +
+    coord_sf(crs = st_crs(4326)) +
+    geom_sf(data = CBD_bdry, fill = NA, aes(col = 'CBD'), inherit.aes = FALSE) +
+    coord_sf(crs = st_crs(4326)) +
+    scale_colour_manual(
+      values =  c('Blue', 'Black', 'Purple', 'Red', 'Green')
+    ) +
+    labs(title = title1,
+         col = 'Legend') +
+    theme(axis.text = element_blank(),
+          rect = element_blank(),
+          axis.title = element_blank(),
+          axis.ticks = element_blank(),
+          legend.position = 'top') +
+    xlim(-93.34, -93.18) +
+    ylim(44.89, 45.05) +
+    annotation_scale(location = 'bl', width_hint = 0.4)
+  
+  legend <- get_legend(p1)
+  
+  p1 <- p1 + theme(legend.position = 'none')
+  
+  ## right figure (without exposure)
+  plot_data <- map %>%
+    filter(rank_no_exp < crit) %>% 
+    mutate(both = ifelse(both == 1, 'Same Location', 'Different Location'))
+    
+  p2 <- get_googlemap('Minneapolis', zoom = 11,
+                      maptype = "roadmap",
+                      style = c(feature = "all", element = "labels", visibility = 'off'),
+                      color = "bw") %>%
+    ggmap() +
+    geom_sf(data = plot_data, aes(col = factor(both)), inherit.aes =  FALSE, size = 1) +
+    coord_sf(crs = st_crs(4326)) +
+    geom_sf(data = city_bdry, fill = NA, aes(col = 'City Boundary'), inherit.aes = FALSE) +
+    coord_sf(crs = st_crs(4326)) +
+    geom_sf(data = acp_bdry, fill = NA, aes(col = 'ACP50'), inherit.aes = FALSE) +
+    coord_sf(crs = st_crs(4326)) +
+    geom_sf(data = CBD_bdry, fill = NA, aes(col = 'CBD'), inherit.aes = FALSE) +
+    coord_sf(crs = st_crs(4326)) +
+    scale_colour_manual(
+      values =  c('Blue', 'Black', 'Purple', 'Red', 'Green')
+    ) +
+    labs(title = title2,
+         col = 'Legend') +
+    theme(axis.text = element_blank(),
+          rect = element_blank(),
+          axis.title = element_blank(),
+          axis.ticks = element_blank(),
+          legend.position = 'none') +
+    xlim(-93.34, -93.18) +
+    ylim(44.89, 45.05) +
+    annotation_north_arrow(location = "bl", which_north = "true",
+                           pad_x = unit(3.7, "in"),
+                           pad_y = unit(5.7, "in"),
+                           style = north_arrow_fancy_orienteering)
+  
+  ## combine two plots
+  g <- grid.arrange(arrangeGrob(p1, p2, nrow = 1), legend, nrow = 2, heights = c(10, 1))
+  
+  ## save plot in two formats (jpg and emf)
+  ggsave(paste('./result/', person, 'in', location, '.jpg'), g, width = 10, height = 8)
+  ggsave(paste('./result/', person, 'in', location, '.emf'), g, width = 10, height = 8)
+}
+
+compare('ped', 'int')
+compare('ped', 'block')
+compare('bike', 'int')
+compare('bike', 'block')
 
 # create intersection and mid-block shp file for plot ---------------------
 
